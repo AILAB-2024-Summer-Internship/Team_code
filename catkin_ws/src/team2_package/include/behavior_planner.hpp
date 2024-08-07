81e968da723d32b4ffb7718e129e24d98f537f52
kookoo9kookoo <139210690+kookoo9kookoo@users.noreply.github.com>
2024-08-08 02:27:48 +0900

    Update behavior_planner.hpp

---
+++ catkin_ws/src/team2_package/include/behavior_planner.hpp
@@ -0,0 +1,17 @@
+#include <ros/ros.h>
+#include <vector>
+#include <carla_msgs/
+class collision_check{
+  private:
+  public:
+road_option 
+LANE FOLLOW STRAIGHT AABB
+LEFT RIGHT CONCAVE POLYGON
+LANE CHANGE LATTICE PLANNER
+x,y compare v * dt + car spec cos,sin(yaw)
+};
+
+bool road_option_check(const int& road_option);
+JUNCTION / INTERSECTION STOP
+
+void traffic_sign_check
