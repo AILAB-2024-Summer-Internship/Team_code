982677db57e278af3ef82f00a6110f93c8e686b2
kookoo9kookoo <139210690+kookoo9kookoo@users.noreply.github.com>
2024-08-10 23:19:52 +0900

    Update global_planner.hpp

--- catkin_ws/src/team2_package/include/global_planner.hpp
+++ catkin_ws/src/team2_package/include/global_planner.hpp
@@ -2,12 +2,10 @@
 #define GLOBAL_PLANNER_HPP
 
 #include <ros/ros.h>
+#include <fstream>
+#include <sstream>
 #include <vector>
-#include <cmath>
-#include <Eigen/Dense>
-#include "spline.h"
-#include <tf/transform_datatypes.h>
-#include <carla_msgs/CarlaRoute.h>
+#include <string>
 #include "team2_package/vehicle_state.h"
 #include "team2_package/globalwaypoints.h"
 
@@ -21,30 +19,22 @@ public:
         waypoint(float x, float y);
     };
 
-    int idx = 0;
-    int endidx = 10;
-
     GlobalPlanner();
 
-    void pose_cb(const team2_package::vehicle_state::ConstPtr& msg);
-    void route_cb(const carla_msgs::CarlaRoute::ConstPtr& msg);
-    void route_interpolate(const std::vector<waypoint>& waypoints, const std::vector<int>& options);
+    void load_csv(const std::string& filename);
     void path_publisher();
 
 private:
     ros::NodeHandle nh;
-    ros::Subscriber pose_sub;
-    ros::Subscriber route_sub;
     ros::Publisher path_pub;
 
     std::vector<float> pose;
     std::vector<int> options;
     std::vector<waypoint> waypoints;
-    std::vector<int> my_options;
-    std::vector<waypoint> my_waypoints;
 
-    const int RESERVE_SIZE = 500;
-    const int MY_SIZE = 15000;
+    int idx = 0;
+    int endidx = 10;
+    const int SIZE = 10000;
 };
 
 #endif
