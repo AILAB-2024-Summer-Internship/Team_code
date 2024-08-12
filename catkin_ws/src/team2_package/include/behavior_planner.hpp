baadf4eff1253836d3726b3f995dededbecaf214
kookoo9kookoo <139210690+kookoo9kookoo@users.noreply.github.com>
2024-08-12 22:06:04 +0900

    Update behavior_planner.hpp

--- catkin_ws/src/team2_package/include/behavior_planner.hpp
+++ catkin_ws/src/team2_package/include/behavior_planner.hpp
@@ -1,52 +1,58 @@
 #include <ros/ros.h>
 #include <vector>
-#include <std_msgs/Float32.h>
 #include "vision_msgs/BoundingBox2DArray.h"
+#include "carla_msgs/CarlaSpeedometer.h"
+#include "team2_package/vehicle_state.h"
+#include <carla_msgs/CarlaEgoVehicleControl.h>
 #include "team2_package/globalwaypoints.h"
-#include <std_msgs/Bool>
+#include "std_msgs/Bool.h"
+#include "std_msgs/Float32.h"
 
-class CollisionCheck{
+class BehaviorPlanner{
   public:
-    struct object{
-        float min_x;
+    struct object{ // vertical y, horizontal x 
         float min_y;
-        float max_x;
+        float min_x;
         float max_y;
+        float max_x;
+        float v_y;
+        float v_x;
 
         object();
-        object(float min_x, float min_y, float max_x, float max_y);
+        object(float min_y, float min_x, float max_y, float max_x, float v_y, float v_x);
     };
 
-    int next_rop;
-    int next2_rop;
-    float speed;
-    bool object_detected = false;
-    bool jcic_stop = false;
-    bool trafficlight_none = true;
-    bool green_light = true;
-    bool local_plan = false;
-    bool global_stop = false;
-
-    CollisionCheck();
+    BehaviorPlanner();
 
     void object_cb(const vision_msgs::BoundingBox2DArray::ConstPtr& msg);
-    void road_option_cb(const team2_package::globalwaypoints::ConstPtr& msg);
     void speed_cb(const carla_msgs::CarlaSpeedometer::ConstPtr& msg);
-    void localplan_cb(const std_msgs::Bool::ConstPtr& msg);
+    void yaw_cb(const team2_package::vehicle_state::ConstPtr& msg);
+    void steering_angle_cb(const carla_msgs::CarlaEgoVehicleControl::ConstPtr& msg);
+    void road_option_cb(const team2_package::globalwaypoints::ConstPtr& msg);
+    // void local_fin_cb(const std_msgs::Bool::ConstPtr& msg);
     // void traffic_sign_cb(const ::ConstPtr& msg);
 
-    void object_detect(const int& next_rop, const std::vector<object>& objects)
-    void junction_intersection(const int& next_rop, const int& next2_rop);
-    bool stop();
-    void global_stop_publisher();
+    void prediction();
+    void collision_check();
+    void AEB();
+    void avoidance();
+    void speed_profile();
+    void publisher();
 
   private:
     ros::NodeHandle nh;
     ros::Subscriber object_sub;
-    ros::Subscriber road_option_sub;
     ros::Subscriber speed_sub;
-    ros::Publisher stop_pub;
-    // ros::Publisher activate_lp_pub;
+    ros::Subscriber yaw_sub;
+    ros::Subscriber steering_angle_sub;
+    // ros::Subscriber local_fin_sub;
+    // ros::Subscriber traffic_sign_sub;
+
+    ros::Publisher AEB_pub;
+    // ros::Publisher local_req_pub;
+    ros::Publisher speed_pub;
+    ros::Publisher ACC_pub;
+    ros::Publisher distance_pub;
 
     std::vector<object> objects;
 };
