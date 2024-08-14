141d5632d4e97f790cd586270867bda08c9b863b
kookoo9kookoo <139210690+kookoo9kookoo@users.noreply.github.com>
2024-08-14 10:01:41 +0900

    Update global_planner_temp.cpp

--- catkin_ws/src/team2_package/src/planning/global_planner_temp.cpp
+++ catkin_ws/src/team2_package/src/planning/global_planner_temp.cpp
@@ -5,6 +5,9 @@
 #include <string>
 #include <ros/ros.h>
 #include <carla_msgs/CarlaRoute.h>
+#include <iomanip>
+#include <spline.h>
+#include <tf/transform_datatypes.h>
 
 struct waypoint {
     float x;
@@ -28,8 +31,8 @@ private:
     std::vector<int> my_options;
     std::vector<waypoint> my_waypoints;
 
-    const int RESERVE_SIZE = 10000;
-    const int MY_SIZE = 10000;
+    const int RESERVE_SIZE = 15000;
+    const int MY_SIZE = 15000;
 };
 
 GlobalPlanner::GlobalPlanner() {
@@ -37,7 +40,6 @@ GlobalPlanner::GlobalPlanner() {
     options.reserve(RESERVE_SIZE);
     waypoints.reserve(RESERVE_SIZE);
     my_options.reserve(MY_SIZE);
-    my_waypoints.reserve(MY_SIZE);
 }
 
 void GlobalPlanner::route_cb(const carla_msgs::CarlaRoute::ConstPtr& msg) {
@@ -59,6 +61,7 @@ void GlobalPlanner::route_interpolate(const std::vector<waypoint>& waypoints, co
 
     for (size_t i = 0; i < waypoints_size - 1; i++) {
         const int& option = options[i];
+        const int& option2 = options[i+1];
         const auto& waypoint1 = waypoints[i];
         const auto& waypoint2 = waypoints[i + 1];
         float x1 = waypoint1.x;
@@ -68,7 +71,6 @@ void GlobalPlanner::route_interpolate(const std::vector<waypoint>& waypoints, co
         float dx = x2 - x1;
         float dy = y2 - y1;
         if ((x1 == x2) && (y1 == y2)) {
-            i++;
         } else {
             if (option == 3 || option == 4) {
                 int num_points = static_cast<int>((std::sqrt(dx * dx + dy * dy)) / 2);
@@ -84,7 +86,60 @@ void GlobalPlanner::route_interpolate(const std::vector<waypoint>& waypoints, co
                     my_options.push_back(option);
                     my_waypoints.push_back(waypoint(x1, y1));
                 }
-            } else {
+            } else if (option == 5 || option == 6) { // cubic spiral로 보간하기
+                const auto& waypoint3 = waypoints[i + 2];
+                float x3 = waypoint3.x;
+                float y3 = waypoint3.y;
+                float ix, iy;
+                float dx2 = x3-x2;
+                float dy2 = y3-y2;
+                float mid_point_x, mid_point_y;
+                std::vector<double> tempX, tempY;
+                tempX.reserve(3);
+                tempY.reserve(3);
+                mid_point_x = (x2 + x1) * 0.5;
+                mid_point_y = (y2 + y1) * 0.5;
+                if (x1 < x2) {
+                    tempX.push_back(x1);
+                    tempX.push_back(mid_point_x);
+                    tempX.push_back(x2);
+                    tempY.push_back(y1);
+                    tempY.push_back(mid_point_y);
+                    tempY.push_back(y2);
+                } else {
+                    tempX.push_back(x2);
+                    tempX.push_back(mid_point_x);
+                    tempX.push_back(x1);
+                    tempY.push_back(y2);
+                    tempY.push_back(mid_point_y);
+                    tempY.push_back(y1);
+                }
+
+                tk::spline s(tempX, tempY);
+                float xm_1 = (x1 + mid_point_x) / 2;
+                float ym_1 = static_cast<float>(s(xm_1));
+                float xm_2 = (x2 + mid_point_x) / 2;
+                float ym_2 = static_cast<float>(s(xm_2));
+                for (int j = 0; j < 5; j++) {
+                    my_options.push_back(option);
+                }
+                my_waypoints.push_back(waypoint(x1, y1));
+                my_waypoints.push_back(waypoint(xm_1, ym_1));
+                my_waypoints.push_back(waypoint(mid_point_x, mid_point_y));
+                my_waypoints.push_back(waypoint(xm_2, ym_2));
+                my_waypoints.push_back(waypoint(x2, y2));
+                int num_points = static_cast<int>((std::sqrt(dx2 * dx2 + dy2 * dy2)) / 2);
+                if (num_points > 1) {
+                    for (int j = 0; j < num_points; j++) {
+                        my_options.push_back(4);
+                        float t = static_cast<float>(j) / num_points;
+                        float ix = x2 + t * dx;
+                        float iy = y2 + t * dy;
+                        my_waypoints.push_back(waypoint(ix, iy));
+                    }
+                }
+                i++;
+            }else {
                 my_options.push_back(option);
                 my_waypoints.push_back(waypoint(x1, y1));
             }
@@ -102,11 +157,12 @@ void GlobalPlanner::save_to_csv(const std::string& filename) {
     }
 
     // CSV 헤더 작성
-    file << "Option, X, Y\n";
+    file << "Option, X, Y, Yaw\n";
+    file << std::fixed << std::setprecision(4);
 
     // my_options 및 my_waypoints 벡터의 데이터를 CSV로 저장
     for (size_t i = 0; i < my_waypoints.size(); ++i) {
-        file << my_options[i] << ", " << my_waypoints[i].x << ", " << my_waypoints[i].y << "\n";
+        file << my_options[i] << ", " << my_waypoints[i].x << ", " << my_waypoints[i].y <<"\n";
     }
 
     file.close();
